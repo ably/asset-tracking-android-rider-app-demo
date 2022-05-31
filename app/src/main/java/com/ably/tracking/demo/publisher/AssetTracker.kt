01abcf52379c5f73fa337be2a74db51deca15627
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-05-31 14:04:13 +0200

    AssetTracker.kt class created

---
+++ app/src/main/java/com/ably/tracking/demo/publisher/AssetTracker.kt
@@ -0,0 +1,50 @@
+package com.ably.tracking.demo.publisher
+
+import android.Manifest
+import android.content.Context
+import androidx.annotation.RequiresPermission
+import com.ably.tracking.Accuracy
+import com.ably.tracking.Resolution
+import com.ably.tracking.connection.Authentication
+import com.ably.tracking.connection.ConnectionConfiguration
+import com.ably.tracking.publisher.*
+
+class AssetTracker(
+    private val mapBoxAccessToken: String,
+    private val ablyClientId: String,
+    private val ablyApiKey: String
+) {
+    private var publisher: Publisher? = null
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    fun connect(context: Context) {
+        // Prepare the default resolution for the Resolution Policy
+        val defaultResolution =
+            Resolution(Accuracy.BALANCED, desiredInterval = 1000L, minimumDisplacement = 1.0)
+
+        publisher = Publisher.publishers() // get the Publisher builder in default state
+            .connection(
+                ConnectionConfiguration(
+                    Authentication.basic(
+                        ablyClientId,
+                        ablyApiKey
+                    )
+                )
+            ) // provide Ably configuration with credentials
+            .map(MapConfiguration(mapBoxAccessToken)) // provide Mapbox configuration with credentials
+            .androidContext(context) // provide Android runtime context
+            .resolutionPolicy(
+                DefaultResolutionPolicyFactory(
+                    defaultResolution,
+                    context
+                )
+            ) // provide either the default resolution policy factory or your custom implementation
+            .profile(RoutingProfile.DRIVING) // provide mode of transportation for better location enhancements
+            .start()
+    }
+
+    suspend fun disconnect() {
+        publisher?.stop()
+        publisher = null
+    }
+}
