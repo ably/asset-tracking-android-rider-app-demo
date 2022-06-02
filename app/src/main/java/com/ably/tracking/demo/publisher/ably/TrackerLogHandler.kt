fd12cbc5a175530d723663751c0acb937e7981e3
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-06-02 11:29:08 +0200

    LogHandler updated to match subscriber implementation

--- app/src/main/java/com/ably/tracking/demo/publisher/ably/TrackerLogHandler.kt
+++ app/src/main/java/com/ably/tracking/demo/publisher/ably/TrackerLogHandler.kt
@@ -6,6 +6,12 @@ import com.ably.tracking.logging.LogLevel
 
 class TrackerLogHandler : LogHandler {
     override fun logMessage(level: LogLevel, message: String, throwable: Throwable?) {
-        Log.d("AssetTracker", "$message $throwable")
+        when (level) {
+            LogLevel.VERBOSE -> Log.v("AssetTracker", "Message: $message, Throwable: $throwable")
+            LogLevel.INFO -> Log.i("AssetTracker", "Message: $message, Throwable: $throwable")
+            LogLevel.DEBUG -> Log.d("AssetTracker", "Message: $message, Throwable: $throwable")
+            LogLevel.WARN -> Log.w("AssetTracker", "Message: $message, Throwable: $throwable")
+            LogLevel.ERROR -> Log.e("AssetTracker", "Message: $message, Throwable: $throwable")
+        }
     }
 }
