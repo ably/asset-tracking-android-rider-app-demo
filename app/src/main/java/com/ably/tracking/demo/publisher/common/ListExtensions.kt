0257ff398ec19440ba78d6237762081cadc6fffc
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-08-04 14:25:30 +0200

    copyAndReplaceElementAt list extension fixed

--- app/src/main/java/com/ably/tracking/demo/publisher/common/ListExtensions.kt
+++ app/src/main/java/com/ably/tracking/demo/publisher/common/ListExtensions.kt
@@ -1,4 +1,4 @@
 package com.ably.tracking.demo.publisher.common
 
 fun <T> List<T>.copyAndReplaceElementAt(index: Int, element: T) =
-    subList(0, index) + element + subList(index, size - 1)
+    subList(0, index) + element + subList(index, size)
---
+++ app/src/test/java/com/ably/tracking/demo/publisher/common/ListExtensionsKtTest.kt
@@ -0,0 +1,22 @@
+package com.ably.tracking.demo.publisher.common
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+internal class ListExtensionsKtTest {
+
+    @Test
+    fun `copyAndReplaceElementAt should alter only element at provided index`() = runTest {
+        // given
+        val input = listOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val output = input.copyAndReplaceElementAt(index = 2, element = 9)
+
+        // then
+        assertThat(output).isEqualTo(listOf(1, 2, 9, 3, 4, 5, 6))
+    }
+}
