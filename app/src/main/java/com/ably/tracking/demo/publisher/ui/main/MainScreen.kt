13deba210e535c3f3750a76fd63c26aa69307f53
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-08-24 13:40:27 +0200

    Spacer added between order id and status

--- app/src/main/java/com/ably/tracking/demo/publisher/ui/main/MainScreen.kt
+++ app/src/main/java/com/ably/tracking/demo/publisher/ui/main/MainScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
@@ -115,6 +117,7 @@ fun OrderRow(order: OrderViewItem) {
             .padding(8.dp)
     ) {
         Text(text = order.name)
+        Spacer(modifier = Modifier.width(8.dp))
         Text(text = stringResource(id = order.state))
         Column {
             StyledTextButton(text = R.string.track_order_button, onClick = order.onTrackClicked)
